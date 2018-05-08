01900cc1d8374cac4c63199dc26c093ae5a40ebc
danivallejo <danivallejo@opendeusto.es>
2018-05-08 18:52:14 +0200

    Estrucutura de la clase tarjeta hecha

---
+++ Tarjeta/Tarjeta.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+Tarjeta::Tarjeta()
+{
+
+}
+void Tarjeta::AltaTarjeta ()
+{
+
+}
+void Tarjeta::IntroducirTarjeta ()
+{
+
+}
+void Tarjeta::ConsultarSaldo()
+{
+
+}
+int getnumTarjeta()
+{
+
+}
+void setnumTarjeta(int numTarjeta)
+{
+
+}
+int getPIN()
+{
+
+}
+void setPIN(int PIN)
+{
+
+}
+int getsaldo()
+{
+
+}
+void setsaldo(int saldo)
+{
+	
+}
+
---
+++ Tarjeta/Tarjeta.h
@@ -0,0 +1,16 @@
+
+class Tarjeta
+{
+	int numTarjeta;
+	int PIN;
+	int saldo;
+
+public:
+	Tarjeta();
+	int AltaTarjeta ();
+	void IntroducirTarjeta ();
+	void ConsultarSaldo();
+	
+
+
+}
