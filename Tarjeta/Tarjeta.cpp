f421bd38aa8e7649eb6cbdb81ebbe32a7aa7baf8
danivallejo <danivallejo@opendeusto.es>
2018-05-09 15:35:44 +0200

    Getters y setters de la clase tarjeta hechos

--- Tarjeta/Tarjeta.cpp
+++ Tarjeta/Tarjeta.cpp
@@ -19,25 +19,29 @@ void Tarjeta::ConsultarSaldo()
 }
 int getnumTarjeta()
 {
-
+return numTarjeta;
 }
 void setnumTarjeta(int numTarjeta)
 {
-
+this-->numTarjeta=numTarjeta;
 }
 int getPIN()
 {
-
+return PIN;
 }
 void setPIN(int PIN)
 {
-
+this-->PIN=PIN;
 }
 int getsaldo()
 {
-
+return saldo;
 }
 void setsaldo(int saldo)
+{
+this-->saldo=saldo;	
+}
+~Tarjeta()
 {
 	
 }
--- Tarjeta/Tarjeta.h
+++ Tarjeta/Tarjeta.h
@@ -11,6 +11,10 @@ public:
 	void IntroducirTarjeta ();
 	void ConsultarSaldo();
 	
-
-
+	int getnumTarjeta();
+	void setnumTarjeta(int numTarjeta);
+	int getPIN();
+	void setPIN(int PIN);
+	int getsaldo();
+	void setsaldo(int saldo);
 }
